21464321b6d1407ab40b45a7b78d09e215ce8129
root <you@example.com>
2024-08-22 10:19:55 +0800

    info

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Example Next.js Notion Starter Site',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
